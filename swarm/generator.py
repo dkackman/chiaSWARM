689fca0bb206f6aab57f65e34efa8d7b9e145e0a
Don Kackman <dkackman@gmail.com>
2023-01-18 21:39:44 -0600

    reduce variable scope

--- swarm/generator.py
+++ swarm/generator.py
@@ -9,13 +9,13 @@ async def do_work(job):
     device = remove_device_from_pool()
 
     try:
-        content_type = job.get("content_type", "image/jpeg")
         id = job.pop("id")
         try:
             kwargs = format_args(job)
             artifacts, pipeline_config = device(**kwargs)  # type: ignore
 
         except Exception as e:
+            content_type = job.get("content_type", "image/jpeg")
             artifacts, pipeline_config = exception_image(e, content_type)
 
         return {
