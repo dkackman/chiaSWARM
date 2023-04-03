499af3f99fca247b02e6e26561d27572017e16d2
Don Kackman <dkackman@gmail.com>
2023-04-03 16:40:17 -0500

    generate image map data

--- swarm/toolbox/stitch.py
+++ swarm/toolbox/stitch.py
@@ -1,12 +1,16 @@
 import math
 import requests
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw
 import io
+import json
 from ..diffusion.output_processor import make_result, make_thumbnail, image_to_buffer
 
+thumb_size = 144
 def stitch_callback(device_id, model_name, **kwargs):
     print("Stitching...")
-    pipeline_config = {}
+    pipeline_config = {
+        "model_name": model_name,
+    }
 
     jobs = kwargs["jobs"]
     image_urls = [job["resultUri"] for job in jobs]
@@ -14,10 +18,12 @@ def stitch_callback(device_id, model_name, **kwargs):
     resized_images = resize_images(images)
     stitched_image = stitch_images(resized_images)
 
-    buffer = image_to_buffer(stitched_image, "image/jpeg", "web_low")
+    buffer = image_to_buffer(stitched_image, "image/jpeg", "web_high")
     thumbnail = make_thumbnail(buffer)
     results = {}
     results["primary"] = make_result(buffer, thumbnail, "image/jpeg")
+    image_map = generate_image_map(resized_images, jobs)
+    pipeline_config["image_map"] = image_map
     return results, pipeline_config
 
 
@@ -29,7 +35,7 @@ def download_images(image_urls):
         images.append(image)
     return images
 
-def resize_images(images, size=(128, 128)):
+def resize_images(images, size=(thumb_size, thumb_size)):
     resized_images = []
     for index, image in enumerate(images):
         resized_image = image.resize(size)
@@ -41,15 +47,36 @@ def resize_images(images, size=(128, 128)):
 
 def stitch_images(resized_images):
     images_per_row = math.ceil(math.sqrt(len(resized_images)))
-    stitched_image_size = 128 * images_per_row
+    stitched_image_size = thumb_size * images_per_row
     stitched_image = Image.new('RGB', (stitched_image_size, stitched_image_size))
 
     x_offset, y_offset = 0, 0
     for img in resized_images:
         stitched_image.paste(img, (x_offset, y_offset))
-        x_offset += 128
+        x_offset += thumb_size
         if x_offset >= stitched_image_size:
             x_offset = 0
-            y_offset += 128
+            y_offset += thumb_size
 
     return stitched_image
+
+def generate_image_map(resized_images, jobs):
+    map_data = []
+    x_offset, y_offset = 0, 0
+    for index, _ in enumerate(resized_images):
+        coords = f"{x_offset},{y_offset},{x_offset + thumb_size},{y_offset + thumb_size}"
+        href = jobs[index]["resultUri"]
+        area_data = {
+            "shape": "rect",
+            "coords": coords,
+            "href": href,
+            "alt": f"Image {index + 1}"
+        }
+        map_data.append(area_data)
+
+        x_offset += thumb_size
+        if x_offset >= thumb_size * math.ceil(math.sqrt(len(resized_images))):
+            x_offset = 0
+            y_offset += thumb_size
+
+    return map_data
