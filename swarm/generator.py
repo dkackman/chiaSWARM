1ac157e130ee4d69780ac0a35b7c44b86b23248c
Don Kackman <dkackman@gmail.com>
2023-04-05 17:50:55 -0500

    indicate a fatal error for parse failure

--- swarm/generator.py
+++ swarm/generator.py
@@ -6,8 +6,30 @@ from . import __version__
 
 async def do_work(job, device):
     id = job.pop("id")
+
     try:
         worker_function, kwargs = format_args(job)
+
+    except Exception as e:
+        # any error here is fatal (i.e. not something a worker could recover from)
+        # the job should not be resubmitted
+        content_type = job.get("content_type", "image/jpeg")
+        print(e)
+        if content_type.startswith("image/"):
+            artifacts, pipeline_config = exception_image(e, content_type)
+        else:
+            artifacts, pipeline_config = exception_message(e)
+
+        return {
+            "id": id,
+            "artifacts": artifacts,
+            "nsfw": pipeline_config.get("nsfw", False),  # type ignore
+            "fatal_error": True,
+            "worker_version": __version__,
+            "pipeline_config": pipeline_config,
+        }            
+
+    try:
         artifacts, pipeline_config = device(worker_function, **kwargs)  # type: ignore
 
     except Exception as e:
