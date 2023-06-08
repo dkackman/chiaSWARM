89d6610f6fd3d7a4eab4d7cd1e5315ee1b1ea8a6
Don Kackman <dkackman@gmail.com>
2023-06-08 23:10:41 +0000

    check for scaled_dot_product_attention  run_compile

--- swarm/type_helpers.py
+++ swarm/type_helpers.py
@@ -1,5 +1,6 @@
 import platform
 import sys
+import torch
 
 is_windows = any(platform.win32_ver())
 is_3_11 = sys.version_info >= (3, 11)
@@ -12,5 +13,5 @@ def get_type(module_name, type_name):
 def has_method(o, name):
     return callable(getattr(o, name, None))
 
-# torch compile not supported on window or 3.11
-run_compile = not (is_windows or is_3_11)
+# torch compile not supported on all platforms
+run_compile = hasattr(torch.nn.functional, "scaled_dot_product_attention") and not (is_windows or is_3_11)
