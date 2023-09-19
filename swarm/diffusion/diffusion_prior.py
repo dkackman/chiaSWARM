6c9a776751c1c2f749a0b9574002386fcab85a25
Don Kackman <dkackman@gmail.com>
2023-09-19 01:58:16 +0000

    kanfinsky controlnet depth img2img

--- swarm/diffusion/diffusion_prior.py
+++ swarm/diffusion/diffusion_prior.py
@@ -7,11 +7,18 @@ def process_prior_pipeline(args, device_identifier):
         pipe_prior = pipeline_prior_type.from_pretrained(
             args.pop("prior_model_name"), torch_dtype=torch.float16
         ).to(device_identifier)
-        image_embeds, negative_image_embeds = pipe_prior(
-            # prompt arguments are consumed by the prior pipeline
-            prompt=args.pop("prompt", None),
-            negative_prompt=args.pop("negative_prompt", None),
-            generator=args["generator"],
-        ).to_tuple()
+
+        if args.pop("split_embeds", False):
+            print("")
+            # image_embeds = pipe_prior(prompt=prompt, image=img, strength=0.85, generator=generator)
+            # negative_image_embeds = pipe_prior(prompt=negative_prior_prompt, image=img, strength=1, generator=generator)
+
+        else:
+            image_embeds, negative_image_embeds = pipe_prior(
+                # prompt arguments are consumed by the prior pipeline
+                prompt=args.pop("prompt", None),
+                negative_prompt=args.pop("negative_prompt", None),
+                generator=args["generator"],
+            ).to_tuple()
         args["image_embeds"] = image_embeds
         args["negative_image_embeds"] = negative_image_embeds
